bbad621153b0f549f6def2533178696a803c4c86
lidong <li757727085@outlook.com>
2023-03-07 09:28:54 +0800

    商品具体列表完成

---
+++ src/main/java/com/heu/miaosha/service/model/OrderModel.java
@@ -0,0 +1,72 @@
+package com.heu.miaosha.service.model;
+
+import java.math.BigDecimal;
+
+/**
+ * @author: lidong
+ * @description
+ * @date: 2023/3/6 21:36
+ */
+public class OrderModel {
+    private String id;
+
+    private Integer userId;
+
+    private Integer itemId;
+
+    private BigDecimal itemPrice;
+
+    private Integer amount;
+
+    private BigDecimal orderPrice;
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getOrderAmount() {
+        return orderPrice;
+    }
+
+    public void setOrderAmount(BigDecimal orderAmount) {
+        this.orderPrice = orderAmount;
+    }
+}
